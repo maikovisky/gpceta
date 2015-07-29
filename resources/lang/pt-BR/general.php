852ae938644cf3ae772103fda519d2724db886a5
Maiko de Andrade <maikovisky@gmail.com>
2015-07-29 01:29:54 -0300

    Add pt-BR lang

---
+++ resources/lang/pt-BR/general.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    */
+
+    'Name'     => "Nome",
+    'name'     => "nome",
+    'New'      => "Novo",
+    'new'      => 'novo',
+    
+    
+    'Projects' => 'Projetos',
+    'projects' => 'projetos',
+    'Project'  => 'Projeto',
+    'project'  => 'projeto',
+    'New_project' => "Novo projeto",
+    'Open_Projects' => "Projetos Abertos",
+    'Start_at'  => 'Inicia em',
+    'Finish_at' => 'Finaliza em',
+    'Finish_preview_at' => 'Previsão de finalizar em',
+    'Description'   => 'Descrição',
+    
+    'Users'    => 'Usuários',
+    'users'    => 'usuários',
+    'User'     => 'Usuário',
+    'user'     => 'usuário',
+    'Team'     => 'Equipe',
+
+    'Resources' => 'Recursos',
+    'Changes'   => 'Mudanças',
+    'Issues'    => 'Questões',
+    
+    'more_info' => 'Mais Informações',
+    
+    'Reservation' => "Reservas",
+    'Room'        => "Sala",
+    'Rooms'       => "Salas",
+    'Car'         => "Carro",
+    'Cars'        => "Carros"
+];
